8ea316d110b367bfa6dcc5b95bf6b7e5ac08ba2f
Prasanjit Pattanaik <me@prasanjitpattanaik.com>
2022-06-10 12:52:33 +0530

    :memo: Added prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  apiBaseUrl: 'https://localhost:7113',
+  //apiBaseUrl: 'https://localhost:7126'
+  emailBaseUrl: 'https://v1.nocodeapi.com/prasanjitpattanaik/zohomail/iwwoWodENuvDiHqq/sendEmail?fromAddress=chubbblog@prasanjitpattanaik.com&'
 };
