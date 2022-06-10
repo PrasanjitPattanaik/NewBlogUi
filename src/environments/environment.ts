6884f2175981248cf52111cadc87c4d99cac3b7d
Prasanjit Pattanaik <me@prasanjitpattanaik.com>
2022-06-10 12:24:28 +0530

    :memo: Updated ENV

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://localhost:7113',
   //apiBaseUrl: 'https://localhost:7126'
-  emailBaseUrl: 'https://v1.nocodeapi.com/prasanjit/zohomail/GjWJlwRJJzzyDkKG/sendEmail?fromAddress=chubbblog@prasanjitpattanaik.com&'
+  emailBaseUrl: 'https://v1.nocodeapi.com/prasanjitpattanaik/zohomail/iwwoWodENuvDiHqq/sendEmail?fromAddress=chubbblog@prasanjitpattanaik.com&'
 };
 
 /*
